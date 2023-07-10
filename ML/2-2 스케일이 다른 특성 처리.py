b5bcf16b086873ba55bfbfa4bb15ab55a88e15d3
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2023-07-10 17:05:23 +0900

    데이터 전처리-표준점수

--- ML/2-2 스케일이 다른 특성 처리.py
+++ ML/2-2 스케일이 다른 특성 처리.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 import numpy as np
-
+print(np.column_stack([[1, 4, 7], [2, 5, 8], [3, 6, 9]]))
 fish_length = [25.4, 26.3, 26.5, 29.0, 29.0, 29.7, 29.7, 30.0, 30.0, 30.7, 31.0, 31.0,
                 31.5, 32.0, 32.0, 32.0, 33.0, 33.0, 33.5, 33.5, 34.0, 34.0, 34.5, 35.0,
                 35.0, 35.0, 35.0, 36.0, 36.0, 37.0, 38.5, 38.5, 39.5, 41.0, 41.0, 9.8,
@@ -11,7 +11,7 @@ fish_weight = [242.0, 290.0, 340.0, 363.0, 430.0, 450.0, 500.0, 390.0, 450.0, 50
                 500.0, 340.0, 600.0, 600.0, 700.0, 700.0, 610.0, 650.0, 575.0, 685.0, 620.0, 680.0,
                 700.0, 725.0, 720.0, 714.0, 850.0, 1000.0, 920.0, 955.0, 925.0, 975.0, 950.0, 6.7,
                 7.5, 7.0, 9.7, 9.8, 8.7, 10.0, 9.9, 9.8, 12.2, 13.4, 12.2, 19.7, 19.9]
-# np.column_stack() -> 입력받은 리스트 들을 일렬로 세우고 차례대로 연결
+# np.column_stack() -> 튜플로 연결된 리스트들을 일렬로 세우고 차례대로 연결 (각 리스트에서 하나씩 뽑아내서 만듬)
 fish_data = np.column_stack((fish_length, fish_weight))
 # np.concatenate() -> 입력받은 리스트 뒤에 리스트를 차례대로 붙임
 fish_target = np.concatenate((np.ones(35), np.zeros(14)))
@@ -25,6 +25,7 @@ kn = KNeighborsClassifier() # n_neighbor 기본값 = 5
 # 평균 계산 / axis = 0 -> 각 열의 평균, axis = 1 -> 각 행의 평균
 mean = np.mean(train_input, axis=0)
 # 표준 편차 계산
+# 평균 과 표준편차 모두 훈련 세트에서 계산
 std = np.std(train_input, axis=0)
 # 훈련 세트 표준 점수 계산 / numpy 는 연산시 모든 데이터에 적용 -> 브로드캐스팅
 train_scaled = (train_input - mean) / std
